5ea105ec31e3f0ebe12f95ca85a523fb587b5b3d
Roger Killer <killerroger4@gmail.com>
2025-12-26 20:48:21 +0100

    Add Docker Compose configuration, integrate dotenv for environment variables, and update server to use dynamic port and app name from .env file.

--- 03-docker-fundametals/src/server.ts
+++ 03-docker-fundametals/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import apiRoutes from "./routes/api.routes";
+import 'dotenv/config'
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
+const APP_NAME = process.env.APP_NAME || 'app'
 
 app.use(express.json());
 app.use("/", apiRoutes);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+  console.log(`🚀 ${APP_NAME} running on port ${PORT}`)
+})
+
